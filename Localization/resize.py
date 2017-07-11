b68cf6d15692686aaf2eeb86002aae2846f85c36
Mohit <diamondsparkmohit@gmail.com>
2017-07-11 10:48:23 -0400

    Support for reading image

--- Localization/resize.py
+++ Localization/resize.py
@@ -1,7 +1,24 @@
 import tensorflow as tf
 import cv2
+import dicom
+import pylab
 
+def readImage(path, file_format):
+    if(file_format.lower()  == "dicom"):
+        ds = dicom.read_file(path)
+        return ds
+    else:
+        img = cv2.imread(path,0)
+        return img
+##        cv2.imshow('image',img)
+##        cv2.waitKey(10)
+##        cv2.destroyAllWindows()
+    
 
-def readImage(path):
-    img = cv2.imread(path,0)
-    #return img
+def displayImage(image):
+    pylab.imshow(image, cmap= pylab.cm.bone)
+    pylab.show()
+
+image = readImage('/Users/mop2014/Downloads/Training/DET0000101/DET0000101_LA1_ph0.png','png')
+print (image)
+displayImage(image)
