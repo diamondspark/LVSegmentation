c028c8982a02544eead5cbd75687db14f476a993
Mohit <diamondsparkmohit@gmail.com>
2017-07-11 12:23:06 -0400

    Support for reading and displaying images

--- Localization/First Contact.py
+++ Localization/First Contact.py
@@ -1,4 +1,16 @@
-import tensorflow as tf
+from image import Image
+import glob
 
+imageList_dcm=[]
+imageList_png=[]
 
+def LoadFromDir(path):
+    for filename in glob.glob(str(path)+'/*.png'):
+         im = Image(filename,'png')
+         imageList_png.append(im)
+    print "Loaded all png"
+    for filename in glob.glob(str(path)+'/*.dcm'):
+         im = Image(filename,'dicom')
+         imageList_dcm.append(im)
 
+LoadFromDir('/Users/mop2014/Downloads/Training/DET0000101')
--- Localization/resize.py
+++ Localization/resize.py
@@ -10,15 +10,12 @@ def readImage(path, file_format):
     else:
         img = cv2.imread(path,0)
         return img
-##        cv2.imshow('image',img)
-##        cv2.waitKey(10)
-##        cv2.destroyAllWindows()
     
-
 def displayImage(image):
     pylab.imshow(image, cmap= pylab.cm.bone)
     pylab.show()
 
-image = readImage('/Users/mop2014/Downloads/Training/DET0000101/DET0000101_LA1_ph0.png','png')
-print (image)
-displayImage(image)
+##image = readImage('/Users/mop2014/Downloads/Training/DET0000101/DET0000101_LA1_ph0.png','png')
+##print (image)
+##displayImage(image)
+i1 = Image
