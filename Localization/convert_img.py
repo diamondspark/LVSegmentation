0b59326a0b50c8eb4f3beec2f1017c8f4eec5360
gtm2122 <gabrielthomas281@gmail.com>
2017-07-25 10:32:17 -0400

    changed convert image

--- Localization/convert_img.py
+++ Localization/convert_img.py
@@ -1,22 +1,21 @@
 from image import readImage
 import os
 
-src = '/data/gabriel/LVseg/dataset/'
+src = '/data/gabriel/LVseg/dataset/Training'
 import scipy.misc
 
 
-for i in os.listdir(src):
-	
-	for j in os.listdir(src+i):
-		for k in os.listdir(src+i+'/'+j):
+#for i in os.listdir(src):
+#	if(i=='Training'):
 
-			if '.dcm' in k:
-				I = readImage(src+i+'/'+j+'/'+k,'dicom')
+for j in os.listdir(src):
+	for k in os.listdir(src+'/'+j):
+		if '.dcm' in k:
+			I = readImage(src+'/'+j+'/'+k,'dicom')
 	
-				scipy.misc.imsave('/data/gabriel/LVseg/dataset_img/img/'+i+'/'+k[:k.find('.')-1]+'.png',I)
-			elif '.png' in k:
-				I = readImage(src+i+'/'+j+'/'+k,'png')
-	
-				scipy.misc.imsave('/data/gabriel/LVseg/dataset_img/label/'+i+'/'+k,I)
-							
+			scipy.misc.imsave('/data/gabriel/LVseg/dataset_img/img/'+'/Training/'+k[:k.find('.')-1]+'.png',I)
+		elif '.png' in k:
+			I = readImage(src+'/'+j+'/'+k,'png')
 
+			scipy.misc.imsave('/data/gabriel/LVseg/dataset_img/label/'+'Training/'+k,I)
+						
